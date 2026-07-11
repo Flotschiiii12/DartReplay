b2e3d659ef49fedaac1489292d1eac4b4597ead2
flotschiiii12 <florianbpod@gmail.com>
2026-07-11 15:14:01 +0200

    Add throw persistence from SCOLIA websocket

--- backend/scolia/client.py
+++ backend/scolia/client.py
@@ -1,26 +1,73 @@
 
 import asyncio
+import json
 import websockets
+import psycopg2
 
 from config import SERIAL_NUMBER, ACCESS_TOKEN
 
 
 async def main():
+    connection = psycopg2.connect(
+        host="localhost",
+        database="dartreplay",
+        user="dartreplay",
+        password="dartreplay123"
+    )
+
+    cursor = connection.cursor()
+
     websocket_url = (
         f"wss://game.scoliadarts.com/api/v1/social"
         f"?serialNumber={SERIAL_NUMBER}"
         f"&accessToken={ACCESS_TOKEN}"
     )
 
-    print("Verbinde mit:")
-    print(websocket_url)
-
     async with websockets.connect(websocket_url) as websocket:
         print("Verbunden!")
 
         while True:
             message = await websocket.recv()
-            print(message)
+
+            data = json.loads(message)
+
+            print(data)
+
+            if data["type"] == "THROW_DETECTED":
+
+                payload = data["payload"]
+
+                cursor.execute(
+                    """
+                    INSERT INTO throws
+                    (
+                        sector,
+                        x,
+                        y,
+                        bounceout,
+                        detection_time
+                    )
+                    VALUES
+                    (
+                        %s,
+                        %s,
+                        %s,
+                        %s,
+                        %s
+                    )
+                    """,
+                    (
+                        payload["sector"],
+                        payload["coordinates"][0],
+                        payload["coordinates"][1],
+                        payload["bounceout"],
+                        payload["detectionTime"]
+                    )
+                )
+
+                connection.commit()
+
+                print("✅ Wurf gespeichert")
 
 
 asyncio.run(main())
